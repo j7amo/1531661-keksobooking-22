882632e2c78035037f3a880567bf576dc9bd80f9
j7amo <roman.tarnakin@gmail.com>
2021-02-08 08:58:50 +0300

    Вносит финальные правки по ревью от 04.02.2021

--- js/main.js
+++ js/main.js
@@ -33,10 +33,6 @@ getRandomFloat();
 
 // ДЗ №3 (module3-task1)
 
-// В файле main.js на основе написанных в прошлом задании утилитарных функций напишите необходимые функции
-// для создания массива из 10 сгенерированных JS-объектов.
-
-// 1) объявим необходимые константы, известные до выполнения
 const GENERATED_OBJECTS_COUNT = 10;
 const OFFER_TITLE = 'Лучшее предложение по аренде недвижимости!';
 const MIN_PRICE = 1;
@@ -58,15 +54,10 @@ const LONGITUDE_START = 139.70000;
 const LONGITUDE_END = 139.80000;
 const PRECISION = 5;
 
-// 2) объявим утилитарную функцию, которая будет возвращать случайный элемент из переданного ей массива
 const getRandomElement = (array) => {
   return array[getRandomInt(0, array.length - 1)];
 };
 
-// 3) объявим утилитарную функцию, которая будет:
-//    - принимать массив;
-//    - возвращать новый случайной длины.
-// При этом элементы массива должны быть уникальны, то есть НЕ повторяться.
 const getRandomLengthArray = (array) => {
   const randomLengthArray = [];
   const randomLength = getRandomInt(1, array.length);
@@ -81,10 +72,8 @@ const getRandomLengthArray = (array) => {
   return randomLengthArray;
 };
 
-// 4) объявим функцию, которая будет возвращать сгенерированный согласно ТЗ объект
 const generateRentOffer = () => {
 
-  // объявим отдельно 2 переменные вне return, так как эти значения требуются в разных свойствах генерируемого объекта
   const randomX = getRandomFloat(LATITUDE_START, LATITUDE_END, PRECISION);
   const randomY = getRandomFloat(LONGITUDE_START, LONGITUDE_END, PRECISION);
 
@@ -112,7 +101,6 @@ const generateRentOffer = () => {
   }
 };
 
-// 5) объявим утилитарную функцию, которая добавит сгенерированные объекты в массив и вернёт его
 const getGeneratedOffers = () => {
   const generatedOffers = [];
 
@@ -123,5 +111,4 @@ const getGeneratedOffers = () => {
   return generatedOffers;
 };
 
-// "приласкаем" линтер вызовом функции
 getGeneratedOffers();
