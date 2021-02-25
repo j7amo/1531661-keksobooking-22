bec8b2a9dfca538c5cb477f68ada753326f2ec23
j7amo <roman.tarnakin@gmail.com>
2021-02-25 14:15:52 +0300

    Вносит первые правки по ревью от 20.02.2021

--- js/similar-objects.js
+++ js/similar-objects.js
@@ -1,8 +1,7 @@
 import { getGeneratedOffers } from './data.js';
 
-const generatedOffers = getGeneratedOffers();
-const offerTemplateContent = document.querySelector('#card').content;
-const offerTemplate = offerTemplateContent.querySelector('.popup');
+const offers = getGeneratedOffers();
+const template = document.querySelector('#card').content.querySelector('.popup');
 
 // словарь видов размещения
 const offerTypes = {
@@ -13,84 +12,84 @@ const offerTypes = {
 };
 
 // функция для проверки присутствия элемента массива удобств в классах элемента списка popup__features
-const checkIfIncluded = (clonedFeature, generatedFeatures) => generatedFeatures.some((element) => clonedFeature.className.includes(element));
+const isIncluded = (clonedFeature, generatedFeatures) => generatedFeatures.some((element) => clonedFeature.className.includes(element));
 
 // функция для проверки элементов списка popup__features
 // если такого удобства в сгенерированном оффере нет - удаляем лишку нафиг
 const checkFeatures = (clonedFeatures, generatedFeatures) => {
   for(let i = clonedFeatures.length - 1; i >= 0; i--) {
-    if(!checkIfIncluded(clonedFeatures[i], generatedFeatures)) {
+    if(!isIncluded(clonedFeatures[i], generatedFeatures)) {
       clonedFeatures[i].remove();
     }
   }
 };
 
 // функция для создания элемента img с правильным src
-const createPhoto = (nodeToAppendPhoto, photoTemplate, generatedPhotos) => {
-  for(let i = 1; i < generatedPhotos.length - 1; i++) {
+const createPhoto = (photosContainer, photoTemplate, photos) => {
+  for(let i = 1; i < photos.length - 1; i++) {
     const popupPhoto = photoTemplate.cloneNode(true);
-    popupPhoto.src = generatedPhotos[i];
-    nodeToAppendPhoto.appendChild(popupPhoto);
+    popupPhoto.src = photos[i];
+    photosContainer.appendChild(popupPhoto);
   }
 };
 
 // функция для создания попапа
-const createPopup = (popupTemplate, generatedOffer) => {
-  const elementFromOfferTemplate = popupTemplate.cloneNode(true);
-  const popupTitle = elementFromOfferTemplate.querySelector('.popup__title');
-  const offerTitle = generatedOffer.offer.title;
+const createPopup = (template, offer) => {
+  const popup = template.cloneNode(true);
+  const popupTitle = popup.querySelector('.popup__title');
+  const offerTitle = offer.offer.title;
   popupTitle.textContent = offerTitle ? offerTitle : popupTitle.remove();
-  const popupTextAddress = elementFromOfferTemplate.querySelector('.popup__text--address');
-  const offerAddress = generatedOffer.offer.address;
+  const popupTextAddress = popup.querySelector('.popup__text--address');
+  const offerAddress = offer.offer.address;
   popupTextAddress.textContent = offerAddress ? offerAddress : popupTextAddress.remove();
-  const popupTextPrice = elementFromOfferTemplate.querySelector('.popup__text--price');
-  const offerPrice = generatedOffer.offer.price;
+  const popupTextPrice = popup.querySelector('.popup__text--price');
+  const offerPrice = offer.offer.price;
   popupTextPrice.textContent = offerPrice ? `${offerPrice} \u20BD/ночь` : popupTextPrice.remove();
-  const popupType = elementFromOfferTemplate.querySelector('.popup__type');
-  const offerType = generatedOffer.offer.type;
+  const popupType = popup.querySelector('.popup__type');
+  const offerType = offer.offer.type;
   popupType.textContent = offerTypes[offerType];
-  const popupTextCapacity = elementFromOfferTemplate.querySelector('.popup__text--capacity');
-  const offerRooms = generatedOffer.offer.rooms;
-  const offerGuests = generatedOffer.offer.guests;
+  const popupTextCapacity = popup.querySelector('.popup__text--capacity');
+  const offerRooms = offer.offer.rooms;
+  const offerGuests = offer.offer.guests;
   popupTextCapacity.textContent = offerRooms && offerGuests ? `${offerRooms} комнаты для ${offerGuests} гостей` : popupTextCapacity.remove();
-  const popupTextTime = elementFromOfferTemplate.querySelector('.popup__text--time');
-  const offerCheckin = generatedOffer.offer.checkin;
-  const offerCheckout = generatedOffer.offer.checkout;
+  const popupTextTime = popup.querySelector('.popup__text--time');
+  const offerCheckin = offer.offer.checkin;
+  const offerCheckout = offer.offer.checkout;
   popupTextTime.textContent = offerCheckin && offerCheckout ? `Заезд после ${offerCheckin}, выезд до ${offerCheckout}` : popupTextTime.remove();
-  const popupFeatures = elementFromOfferTemplate.querySelector('.popup__features');
+  const popupFeatures = popup.querySelector('.popup__features');
 
-  checkFeatures(popupFeatures.children, generatedOffer.offer.features);
+  checkFeatures(popupFeatures.children, offer.offer.features);
 
-  const popupDescription = elementFromOfferTemplate.querySelector('.popup__description');
-  const offerDescription = generatedOffer.offer.description;
+  const popupDescription = popup.querySelector('.popup__description');
+  const offerDescription = offer.offer.description;
   popupDescription.textContent = offerDescription ? offerDescription : popupDescription.remove();
-  const popupPhotos = elementFromOfferTemplate.querySelector('.popup__photos');
+  const popupPhotos = popup.querySelector('.popup__photos');
   const popupPhotoFirst = popupPhotos.querySelector('.popup__photo');
-  const offerPhotos = generatedOffer.offer.photos;
+  const offerPhotos = offer.offer.photos;
   popupPhotoFirst.src = offerPhotos[0] ? offerPhotos[0] : popupPhotoFirst.remove();
 
-  createPhoto(popupPhotos, popupPhotoFirst, generatedOffer.offer.photos);
+  createPhoto(popupPhotos, popupPhotoFirst, offer.offer.photos);
 
-  const popupAvatar = elementFromOfferTemplate.querySelector('.popup__avatar');
-  const offerAvatar = generatedOffer.author.avatar;
+  const popupAvatar = popup.querySelector('.popup__avatar');
+  const offerAvatar = offer.author.avatar;
   popupAvatar.src = offerAvatar ? offerAvatar : popupAvatar.remove();
 
-  return elementFromOfferTemplate;
+  return popup;
 };
 
 // функция для создания массива попапов
-const createPopupArrayFromGeneratedOffers = (generatedOffers) => {
+const createPopupArrayFromGeneratedOffers = (offers) => {
   const popupArray = [];
 
-  for(let i = 0; i < generatedOffers.length; i++) {
-    const popup = createPopup(offerTemplate, generatedOffers[i]);
+  for(let i = 0; i < offers.length; i++) {
+    const popup = createPopup(template, offers[i]);
     popupArray.push(popup);
   }
 
   return popupArray;
 };
 
-const popups = createPopupArrayFromGeneratedOffers(generatedOffers);
+const popups = createPopupArrayFromGeneratedOffers(offers);
 
 const mapCanvas = document.querySelector('#map-canvas');
 mapCanvas.appendChild(popups[0]);
