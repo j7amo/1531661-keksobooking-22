4d551175f9885ca92d11d90e57b83f56ce8b13ff
j7amo <roman.tarnakin@gmail.com>
2021-03-11 09:04:09 +0300

    Вносит изменения в main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 // точка входа, связывающая другие модули
+import { getFixedLengthArrayOfRandomElements } from './util.js';
 import {
   initializeForms,
-  enableForms,
+  enableAdForm,
+  enableMapFiltersForm,
+  addMapFiltersChangeListener,
+  getFiltersValues,
   disableForms,
   initializeAddressInputField,
   addMoveEndListenerToMarker,
@@ -10,15 +14,18 @@ import {
   resetForms
 } from './forms.js';
 import { initializeMap, addOffersMarkersToMap, resetMainPinMarker } from './map.js';
-import { createMapOfPopupsWithCoordinates } from './similar-objects.js';
+import { filterOffers, createMapOfPopupsWithCoordinates } from './similar-objects.js';
 import { getDataFromServer, sendDataToServer, showAlert, showSuccessMessage, showFailMessage } from './server-api.js';
+
+const OFFERS_COUNT = 10;
+let offers;
 
 // теперь попробуем собрать из этих кубиков работающую программу
 initializeForms();
 disableForms();
 const [map, mainPinMarker] = initializeMap();
 map.whenReady(() => {
-  enableForms();
+  enableAdForm();
   addAdFormResetListener(() => {
     resetMainPinMarker();
     setTimeout(() =>
@@ -39,9 +46,24 @@ map.whenReady(() => {
       form);
   });
   getDataFromServer(
-    json => json,
+    (json) => {
+      addMapFiltersChangeListener(() => {
+        const filtersValues = getFiltersValues();
+        // пока оставил код для фильтрации с учётом рейтинга (закомментированная строка) до ответа авторов по ТЗ
+        // const sortedOffers = offers.sort(getSortOffersFunction(getOfferRating, filtersValues)).slice(0, OFFERS_COUNT);
+        const filteredOffers = filterOffers(offers, filtersValues);
+        const mapOfPopups = createMapOfPopupsWithCoordinates(filteredOffers);
+        addOffersMarkersToMap(mapOfPopups, map);
+      });
+      enableMapFiltersForm();
+      return json;
+    },
     () => showAlert('При получении данных с сервера произошла ошибка'),
   )
-    .then(json => createMapOfPopupsWithCoordinates(json))
+    .then(json => {
+      offers = json;
+      const randomOffers = getFixedLengthArrayOfRandomElements(offers, OFFERS_COUNT);
+      return createMapOfPopupsWithCoordinates(randomOffers);
+    })
     .then(popupsWithCoordinates => addOffersMarkersToMap(popupsWithCoordinates, map));
 });
