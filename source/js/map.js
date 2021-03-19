687727470e0373690640f5ecbc200c5b7884e6aa
j7amo <roman.tarnakin@gmail.com>
2021-03-19 09:39:32 +0300

    Вносит изменения в URL-ы главной и второстепенной меток карты

--- source/js/map.js
+++ source/js/map.js
@@ -11,13 +11,13 @@ const mapCanvas = document.querySelector('.map__canvas');
 const additionalMarkers = [];
 
 const mainPinIcon = L.icon({
-  iconUrl: '../img/main-pin.svg',
+  iconUrl: '../build/img/main-pin.svg',
   iconSize: [MAIN_PIN_WIDTH, MAIN_PIN_HEIGHT],
   iconAnchor: [MAIN_PIN_WIDTH / 2, MAIN_PIN_HEIGHT],
 });
 
 const additionalPinIcon = L.icon({
-  iconUrl: '../img/pin.svg',
+  iconUrl: '../build/img/pin.svg',
   iconSize: [ADDITIONAL_PIN_WIDTH, ADDITIONAL_PIN_HEIGHT],
   iconAnchor: [ADDITIONAL_PIN_WIDTH / 2, ADDITIONAL_PIN_HEIGHT],
 });
